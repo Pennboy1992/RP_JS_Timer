6accdd1c5239263f3b721d42b6d8486d2ddff113
Mad Marine <pnnroman92@gmail.com>
2020-03-01 17:07:43 -0600

    hooked up all of our callback methods

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ class Timer {
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
-    if (callbacks) this.onStart = callbacks.onStart;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
@@ -17,7 +21,17 @@ class Timer {
   }
 
   tick = () => {
-    this.timeRemaining <= 0 ? this.pause() : this.timeRemaining = this.timeRemaining - 1;
+    if (this.timeRemaining <= 0) {
+      this.pause()
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
   }
 
   get timeRemaining() {
@@ -46,6 +60,6 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
 
   },
   onComplete() {
-
+    console.log('Timer is completed')
   }
 });
