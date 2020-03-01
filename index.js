a0cd7933391091e00940a072dd8a3680992890d0
Mad Marine <pnnroman92@gmail.com>
2020-03-01 16:34:32 -0600

    implemented automatic pause for when the timer reaches zero

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Timer {
   }
 
   tick = () => {
-    this.timeRemaining = this.timeRemaining - 1;
+    this.timeRemaining <= 0 ? this.pause() : this.timeRemaining = this.timeRemaining - 1;
   }
 
   get timeRemaining() {
