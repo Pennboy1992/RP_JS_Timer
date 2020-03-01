57e56cb532f832a644605c47109947307dd7fb88
Mad Marine <pnnroman92@gmail.com>
2020-03-01 15:19:17 -0600

    added eventlisteners and queryselectors for timer

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ class Timer {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener('click', this.start);
+  }
+
+  start() {
+    console.log('Time to Start!!')
   }
 }
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
