a23d71bbb3d69222b32cc49db981a72ff45fe74b
Mad Marine <pnnroman92@gmail.com>
2020-03-01 16:07:32 -0600

    timer is working in the classgit add .

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
   }
 
-  start() {
-    console.log('Time to Start!!')
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  }
+
+  tick = () => {
+    console.log('tick');
+  }
+
+  pause = () => {
+    clearInterval(this.interval);
   }
 }
 
